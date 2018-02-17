2c1b297f9f693a2223a5c7af330e9c1f9b0e29ed
ryanpradnya <ryanpradnya@gmail.com>
2018-02-17 17:20:32 +0700

    perbaikan login

--- src/main/java/id/co/dropshipper/WebSecurityConfig.java
+++ src/main/java/id/co/dropshipper/WebSecurityConfig.java
@@ -1,11 +1,18 @@
 //package id.co.dropshipper;
 //
+//import javax.persistence.EntityManagerFactory;
+//
+//import java.util.List;
+//
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//import id.co.dropshipper.dao.UserDAO;
+//import id.co.dropshipper.model.User;
 //
 //@Configuration
 //@EnableWebSecurity
@@ -15,7 +22,7 @@
 //
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/", "/login", "/user/daftar").permitAll().anyRequest().authenticated()
+//		http.authorizeRequests().antMatchers("/", "/login", "/user/daftar", "/awal", "/css/**", "/js/**", "/fonts/**", "/images/**", "/sass/**").permitAll().anyRequest().authenticated()
 //				.and().formLogin().defaultSuccessUrl("/user/barang.html", true).loginPage("/login").permitAll().and()
 //				.logout().permitAll();
 //	}
